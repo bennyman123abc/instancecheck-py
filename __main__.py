e436843a86ec69312979c544eb7e678a05bf30e2
bennyman123abc <bennyman123abc@hotmail.com>
2017-09-11 10:59:27 -0400

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,19 @@
+import os
+
+def strCheck(string):
+    if instanceof(string, str):
+        return True
+    else:
+        return False
+        
+def intCheck(integer):
+    if instanceof(integer, int):
+        return True
+    else:
+        return False
+        
+def listCheck(lst):
+    if instanceof(lst, list):
+        return True
+    else:
+        return False
